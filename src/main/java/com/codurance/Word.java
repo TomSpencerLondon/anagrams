6db88ebbde9ff8ffa55b70b901fefea0004c1fca
Tom <thomas.spencer@codurance.com>
2020-09-03 22:59:08 +0100

    Add psvm for testing

--- src/main/java/com/codurance/Word.java
+++ src/main/java/com/codurance/Word.java
@@ -5,6 +5,10 @@ import java.util.Set;
 
 public class Word {
 
+  public static void main(String[] args) {
+    new Word("abc").getAnagrams();
+  }
+
   private final String input;
 
   public Word(String input) {
@@ -20,6 +24,7 @@ public class Word {
       char c = input.charAt(i);
       StringBuilder stringBuilder = new StringBuilder(input);
       String restOfCharacters = stringBuilder.deleteCharAt(i).toString();
+
       Set<Word> anagramsOfRestOfCharacters = new Word(restOfCharacters).getAnagrams();
       anagramsOfRestOfCharacters.stream()
           .map(w -> w.prepend(c))
