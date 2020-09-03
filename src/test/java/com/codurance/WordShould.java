72cc8b89ce2ea60ec7d0bcb487c4a516f30b161a
Tom <thomas.spencer@codurance.com>
2020-09-03 07:32:30 +0100

    Change to Collection

--- src/test/java/com/codurance/WordShould.java
+++ src/test/java/com/codurance/WordShould.java
@@ -2,6 +2,7 @@ package com.codurance;
 
 import static org.assertj.core.api.BDDAssertions.then;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Set;
 import org.junit.jupiter.api.Disabled;
@@ -11,26 +12,26 @@ public class WordShould {
 
   @Test
   void be_its_own_anagram_if_empty() {
-    Set<Word> anagrams = getAnagrams("");
+    Collection<Word> anagrams = getAnagrams("");
     then(anagrams).extracting(Word::toString).containsOnly("");
   }
 
   @Test
   void be_its_own_anagram_if_one_letter_long() {
-    Set<Word> anagrams = getAnagrams("a");
+    Collection<Word> anagrams = getAnagrams("a");
     then(anagrams).extracting(Word::toString).containsOnly("a");
   }
 
   @Test
   void have_itself_and_its_reverse_as_its_only_anagram() {
-    Set<Word> anagrams = getAnagrams("ab");
+    Collection<Word> anagrams = getAnagrams("ab");
     then(anagrams).extracting(Word::toString).containsOnly("ab", "ba");
   }
 
   @Disabled
   @Test
   void have_six_anagrams_for_three_unique_letters() {
-    Set<Word> anagrams = getAnagrams("abc");
+    Collection<Word> anagrams = getAnagrams("abc");
     then(anagrams).extracting(Word::toString)
         .containsOnly(
             "abc", "acb",
@@ -40,7 +41,7 @@ public class WordShould {
 
   }
 
-  private Set<Word> getAnagrams(String input) {
+  private Collection<Word> getAnagrams(String input) {
     return new Word(input).getAnagrams();
   }
 }
